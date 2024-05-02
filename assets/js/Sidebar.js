453c750b51b8e08a7b8e6870187d4b9d2a09b37a
bado19 <64170031+bado19@users.noreply.github.com>
2024-05-02 10:30:14 -0600

    n

---
+++ assets/js/Sidebar.js
@@ -0,0 +1,17 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById("mySidebar");
+    var main = document.getElementById("main");
+    var toggleButton = document.getElementById("sidebarToggle");
+
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        main.style.marginLeft = "0";
+        toggleButton.style.marginLeft = "0"; // Adjust button's margin
+        toggleButton.textContent = "\u2630 Open Sidebar"; // Unicode for hamburger icon
+    } else {
+        sidebar.style.width = "250px";
+        main.style.marginLeft = "250px";
+        toggleButton.style.marginLeft = "250px"; // Adjust button's margin
+        toggleButton.textContent = "\u2715 Close Sidebar"; // Unicode for close icon
+    }
+}
